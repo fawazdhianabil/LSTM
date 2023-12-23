144076e0ce1b1b71006a7fb121a5ec1ef3791cbb
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2023-12-23 12:22:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from sklearn.metrics import f1_score, recall_score, precision_score, confusion_m
 from sklearn.preprocessing import LabelEncoder
 import tensorflow
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D
+from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D,Dropout
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import EarlyStopping
