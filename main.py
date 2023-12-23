c2036b2f85f9055643bd185fc5979f71bcbeda08
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2023-12-23 12:01:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def main():
             data_file = st.file_uploader("Upload CSV file",type=["csv"])            
             if data_file is not None :
                 df = pd.read_csv(data_file)
+                df = df[:100]
                 st.dataframe(df)
 
                 proses = st.button('Start process')
@@ -438,7 +439,8 @@ def main():
 
             # ... existing code ...
 
-        except:
+        except Exception as e:
+            st.write(e)
             st.write('Select The Correct File')
 
 if __name__ == '__main__':
