c252b76b0cd04344961e37097484a2b6e5e2e6ef
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2024-01-12 09:06:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def main():
             data_file = st.file_uploader("Upload CSV file",type=["csv"])            
             if data_file is not None :
                 df = pd.read_csv(data_file)
-                df = df[:10]
+                df = df[:100]
                 st.dataframe(df)
 
                 proses = st.button('Start process')
